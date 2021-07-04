149dee9d32a2fbfa47b02ed3d4ae34056834029b
Taka Oyama <taka.oyama@gmail.com>
2021-07-04 21:31:06 +0900

    add blank decoder for wildcard

--- src/Http/HttpInitializer.php
+++ src/Http/HttpInitializer.php
@@ -5,7 +5,7 @@ namespace Kirameki\Http;
 use Kirameki\Core\Application;
 use Kirameki\Core\InitializerInterface;
 use Kirameki\Http\Codecs\Decoders\JsonDecoder;
-use Kirameki\Http\Codecs\Decoders\NullDecoder;
+use Kirameki\Http\Codecs\Decoders\BlankDecoder;
 use Kirameki\Http\Routing\Router;
 
 class HttpInitializer implements InitializerInterface
@@ -19,10 +19,11 @@ class HttpInitializer implements InitializerInterface
         $app->singleton(HttpHandler::class, function (Application $app) {
             $router = $app->get(Router::class);
             $config = $app->config('http');
-            $handler = new HttpHandler($app, $router, $config);
 
-            $handler->registerEncoder('application/x-www-form-urlencoded', fn() => new NullDecoder);
-            $handler->registerEncoder('application/json', fn() => new JsonDecoder);
+            $handler = new HttpHandler($app, $router, $config);
+            $handler->registerDecoder('*/*', fn() => new BlankDecoder);
+            $handler->registerDecoder('application/x-www-form-urlencoded', fn() => new BlankDecoder);
+            $handler->registerDecoder('application/json', fn() => new JsonDecoder);
 
             return $handler;
         });
