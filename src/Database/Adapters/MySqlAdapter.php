94e26c7ae55273e932a4c4b5e8a4d96d3dd24383
Taka Oyama <toyama4649@hotmail.com>
2022-03-19 21:39:14 +0900

    Revert "run MySQL in strict mode"
    This reverts commit 3c33fc4be45549139ee6f316997c918276a2d719.

--- src/Database/Adapters/MySqlAdapter.php
+++ src/Database/Adapters/MySqlAdapter.php
@@ -37,7 +37,6 @@ class MySqlAdapter extends PdoAdapter
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::MYSQL_ATTR_FOUND_ROWS => true,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode='STRICT_ALL_TABLES'",
         ];
         $this->pdo = new PDO($dsn, $username, $password, $options);
         return $this;
