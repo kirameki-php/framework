851c5cb44f433e68c488b95a6eeb5323857f7e0e
Taka Oyama <taka.oyama@gmail.com>
2024-06-03 07:54:34 +0900

    clean up

--- src/Support/_helpers.php
+++ src/Support/_helpers.php
@@ -1,18 +1,5 @@
 <?php declare(strict_types=1);
 
-use Kirameki\Support\Collection;
-
-/**
- * @template TKey of array-key
- * @template TValue
- * @param iterable<TKey, TValue>|null $items
- * @return Collection<TKey, TValue>
- */
-function collect(?iterable $items = null): Collection
-{
-    return new Collection($items);
-}
-
 /**
  * @param string|object $class
  * @return string
