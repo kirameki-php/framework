55ede1685dfe10275b3e55495c46fb2f6e45c2b1
Taka Oyama <taka.oyama@gmail.com>
2024-06-03 07:30:54 +0900

    redis moved

--- src/Redis/Exceptions/RedisException.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Kirameki\Redis\Exceptions;
-
-use RuntimeException;
-
-class RedisException extends RuntimeException
-{
-}
--- tests/src/Redis/RedisTestCase.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Tests\Kirameki\Redis;
-
-use Tests\Kirameki\TestCase;
-
-class RedisTestCase extends TestCase
-{
-}
